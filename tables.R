759e12e0035478640cad9168e762ee1a5e901e89
Yu Feng <fengyu8299@gmail.com>
2016-09-07 02:12:58 -0500

    benchmarks

--- tables.R
+++ tables.R
@@ -3,6 +3,12 @@ library(dplyr)
 library(tidyr)
 library(MorpheusData)
 
+savefile <- function(df, name) {
+    rawname = paste("data-raw/", name, ".csv", sep='')
+    write.csv(df, file = rawname,  row.names = FALSE)
+    save(df, file = paste("data/", name, ".rdata", sep=''))
+}
+
 #b31_input1 <- read.csv("data-raw/b31_input1.csv")
 #b31_input2 <- read.csv("data-raw/b31_input2.csv")
 #b31_output1 <- read.csv("data-raw/b31_output1.csv")
@@ -28,6 +34,9 @@ b4_input1 <- read.table(textConnection(
                 ALB  Albania        8,097   8,986   10,058  11,123  12,246"), header=TRUE)
 
 b4_output1 <- gather(b4_input1, year, value, -c(Code, Country))
+savefile(b4_input1, "b4_input1")
+savefile(b4_output1, "b4_output1")
+
 
 ############benchmark 5
 b5_input1 <- read.table(textConnection(
@@ -39,6 +48,9 @@ b            N1     343.23     descr1     T2
 b            N2     281.13     descr2     T2
 b            N3     901.23     descr3     T2"), header=TRUE)
 b5_output1 <- spread(b5_input1[-1], treatment, response)
+savefile(b5_input1, "b5_input1")
+savefile(b5_output1, "b5_output1")
+
 
 #############benchmark 7
 b7_input1 <- data.frame(round = c("round1", "round2", "round1", "round2"),
@@ -50,11 +62,8 @@ b7_input1 <- data.frame(round = c("round1", "round2", "round1", "round2"),
 b7_output1 <- gather(b7_input1, variable, value, var1, var2, val) %>%
                     unite(var, variable, round) %>%
                     spread(var, value)
-write.csv(b7_input1, file = "data-raw/b7_input1.csv", row.names = FALSE)
-save(b7_input1, file = "data/b7_input1.rdata")
-write.csv(b7_output1, file = "data-raw/b7_output1.csv", row.names = FALSE)
-save(b7_output1, file = "data/b7_output1.rdata")
-
+savefile(b7_input1, "b7_input1")
+savefile(b7_output1, "b7_output1")
 
 ############benchmark 19
 b19_input1 = structure(list(date = structure(c(1L, 1L, 1L, 1L, 1L, 1L, 1L, 
@@ -66,10 +75,16 @@ b19_input1 = structure(list(date = structure(c(1L, 1L, 1L, 1L, 1L, 1L, 1L,
 -12L))
 
 b19_output1 <- b19_input1 %>% spread(hour, Value)
+savefile(b19_input1, "b19_input1")
+savefile(b19_output1, "b19_output1")
+
 
 ############benchmark 22
 b22_input1 <- data.frame(variable=c(rep('Males', 10), rep('Females', 10)), value=sample(1:1000, 20))
 b22_output1 <- b22_input1 %>% group_by(variable) %>% summarise(mean=mean (value)) 
+savefile(b22_input1, "b22_input1")
+savefile(b22_output1, "b22_output1")
+
 
 #############benchmark 25
 library(lubridate)
@@ -83,6 +98,9 @@ b25_input1 <- data.frame(
 
 b25_output1 <- b25_input1 %>% separate(topics,into=c("key", "value")) %>% 
                                 gather(key1,value2, 4:5) %>% select(-c(4))
+savefile(b25_input1, "b25_input1")
+savefile(b25_output1, "b25_output1")
+
 
 #############benchmark 28
 b28_input1 <- data.frame(month=rep(1:3,2),
@@ -93,11 +111,17 @@ b28_input1 <- data.frame(month=rep(1:3,2),
 b28_output1 <-  b28_input1 %>% gather(variable, value, -(month:student)) %>%
                 unite(temp, student, variable) %>%
                 spread(temp, value)
+savefile(b28_input1, "b28_input1")
+savefile(b28_output1, "b28_output1")
+
 
 #############benchmark 29
 b29_input1 <- structure(list(name = c("A", "A", "A", "B", "B", "B"), information = c("300 USD", 
 "70 kg", "2 cm", "400 USD", "90 kg", "5 cm")), .Names = c("name", 
 "information"), class = "data.frame", row.names = c(NA, -6L))
 b29_output1 <- b29_input1 %>% separate(information, c("tmp1","tmp2")) %>% 
-spread(tmp2, tmp1) %>% select(c(1,4,3,2))
+                spread(tmp2, tmp1) %>% select(c(1,4,3,2))
+savefile(b29_input1, "b29_input1")
+savefile(b29_output1, "b29_output1")
+
 
