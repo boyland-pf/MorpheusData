1b2408ecd7dd955954c5ced72f7a6aa37a99b82f
Yu Feng <fengyu8299@gmail.com>
2016-10-26 23:30:17 -0500

    fix data

--- P18.R
+++ P18.R
@@ -4,9 +4,14 @@ library(tidyr)
 library(MorpheusData)
 
 #############benchmark 1
-dat <- data.frame(message.id = c(1,1,2,3,3),
-                 sender = c("C","A","D","B","B"),
-                 recipient = c("B","C","B","A","D"))
+#original version.
+#dat <- data.frame(message.id = c(1,1,2,3,3),
+#                 sender = c("C","A","D","B","B"),
+#                 recipient = c("B","C","B","A","D"))
+
+dat <- data.frame(message.id = c(1,1,2,3,3,3),
+                 sender = c("A","A","A","B","C","D"),
+                 recipient = c("A","C","B","C","D","B"))
 
 # data to reproduce unsoundness of spec3.
 #dat <- data.frame(message.id = c(1,2,3),
