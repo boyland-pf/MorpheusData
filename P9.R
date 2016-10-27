f61d675d75f7fed33c626eb343a3b93a776f9cfb
Yu Feng <fengyu8299@gmail.com>
2016-10-27 18:07:20 -0500

    p9

--- P9.R
+++ P9.R
@@ -5,20 +5,31 @@ library(MorpheusData)
 
 #############benchmark 1
 dat <- read.table(text=
-"Category     Frequency
-First        10
-First        15
-First        5
-Second       2
-Third        14
-Third        20
-Second       3
+"V1 V2   V3          V4
+a  1 High -0.62645381
+a  2 High  0.18364332
+a  3 High -0.83562861
+a  4 High  1.59528080
+a  1  Low  0.32950777
+a  2  Low -0.82046838
+a  3  Low  0.48742905
+a  4  Low  0.73832471
+b  1 High  0.57578135
+b  2 High -0.30538839
+b  3 High  1.51178117
+b  4 High  0.38984324
+b  1  Low -0.62124058
+b  2  Low -2.21469989
+b  3  Low  1.12493092
+b  4  Low -0.04493361
 ", header=T)
 
 write.csv(dat, "data-raw/p9_input1.csv", row.names=FALSE)
 
-df_out = dat %>% group_by(Category) %>% 
-summarize(Freq = sum(Frequency))
+df_out = dat %>% spread(V3,V4) %>%
+  mutate(Ratio = High/Low) %>%
+  select(V1, V2, Ratio)
+
 
 write.csv(df_out, "data-raw/p9_output1.csv", row.names=FALSE)
 
