02539198ee4dc3967006d1652806f918dc90bdab
Yu Feng <fengyu8299@gmail.com>
2016-11-01 12:43:49 -0500

    working on p7

--- P7.R
+++ P7.R
@@ -4,17 +4,23 @@ library(tidyr)
 library(MorpheusData)
 
 #############benchmark 1
-#dat <- subset(mtcars, gear>3 & cyl >= 4 & hp > 100)
+
 dat <- read.table(text=
-"GeneID     D.1     T.1      D.8    
-A2M  18 50 A1 
-ABL1 20 48 C1
-ACP1 16 8  E1
+"GeneID     D.1     T.1  D.2 T.2   D.8    
+A2M  18 50 2  6 A1 
+ABL1 20 48 4  8 C1
 ", header=T)
+
+#dat <- read.table(text=
+#"GeneID     D.1     T.1      D.8    
+#A2M  18 50 A1 
+#ABL1 20 48 C1
+#ACP1 16 8  E1
+#", header=T)
 
 write.csv(dat, "data-raw/p7_input1.csv", row.names=FALSE)
 
-df_out = dat %>% gather(pt.num.type, value, 2:3) %>%
+df_out = dat %>% gather(pt.num.type, value, 2:5) %>%
      separate(pt.num.type, c("type", "pt.num")) %>%
      spread(type, value) %>%
      mutate(Ratio = D/T) %>% select(-`D.8`)
