76d8602e2d63b308797ec67a147e9b55526bf59e
Ruben Martins <rmartins@cs.utexas.edu>
2016-11-07 00:25:58 -0600

    Updated benchmark 88

--- P88.R
+++ P88.R
@@ -5,19 +5,16 @@ library(MorpheusData)
 
 #############benchmark 88
 dat <- read.table(text=
-"id       time     Q3.1     Q3.2     
-  1 2009-01-01  0.48742885 -0.01618826
-  2 2009-01-02  0.73832471  0.94383621 
-  3 2009-01-03  0.57578135  0.82122120 
-  4 2009-01-04 -0.30538839  0.59388132", header=T)
+"id       time     Q1.1     Q1.2        Q2.1       Q2.2
+  1 2009-01-01  0.48742885 -0.01618826  1.52718069  -0.29177677
+  2 2009-01-02  0.73832471  0.94383621  -0.40380049 -1.19813815", header=T)
 
 write.csv(dat, "data-raw/p88_input1.csv", row.names=FALSE)
 
-  df_out = dat %>%
-     gather(loop_number, "Q3", -`id`, -`time`) %>% 
-     separate(loop_number,c("L1", "L2")) %>% 
-     spread(L1, Q3) %>%
-     select(-L2)
+	  df_out = dat %>% gather(loop, value, -`id`, -`time`) %>% 
+	  		   separate(loop,c("L1","L2")) %>% 
+	  		   spread(L1,value) %>% 
+	  		   select(-L2)
 
 write.csv(df_out, "data-raw/p88_output1.csv", row.names=FALSE)
 
