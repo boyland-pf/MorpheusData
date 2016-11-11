fde38c4d1439974094d94bee9d923163cfc397a0
Ruben Martins <rmartins@cs.utexas.edu>
2016-11-10 18:30:23 -0600

    Minor

--- P67.R
+++ P67.R
@@ -8,14 +8,14 @@ library(data.table)
 dat <- read.table(text=
 " id time
    1    1
-   3    1
-   3    2
-   3    3
+   1    2
+   1    3
    2    1
+   2    2
    4    1
    4    2
    4    3
-   2    2
+   3    1
 ", header=T)
   
 
