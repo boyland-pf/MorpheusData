eaad633fa0cb1bb35532808c51deb42930834608
Yu Feng <fengyu8299@gmail.com>
2016-11-12 15:31:54 -0600

    update 33

--- P33.R
+++ P33.R
@@ -6,17 +6,17 @@ library(MorpheusData)
 #############benchmark 1
 dat <- read.table(text=
 "
-CA UNIT  DATE_1    TIME_1   DESC_1 ENTRIES_1  EXITS_1  DATE_2    TIME_2     DESC_2  ENTRIES_2  EXITS_2
-A002 R051 07-27-13 00:00:00 REGULAR  4209603 1443585 07-27-13 08:00:00   REGULAR  4209663 1443616 
-A002 R051 07-28-13 08:00:00 REGULAR  4210490 1443821 07-28-13 16:00:00   REGULAR  4210775 1443921 
-A002 R051 07-29-13 16:00:00 REGULAR  4211586 1444302 07-30-13 00:00:00   REGULAR  4212845 1444369
+CA DATE_1    TIME_1   DESC_1 ENTRIES_1  EXITS_1  DATE_2    TIME_2     DESC_2  ENTRIES_2  EXITS_2
+A002 07-27-13 00:00:00 REGULAR  4209603 1443585 07-27-13 08:00:00   REGULAR  4209663 1443616 
+A002 07-28-13 08:00:00 REGULAR  4210490 1443821 07-28-13 16:00:00   REGULAR  4210775 1443921 
+A002 07-29-13 16:00:00 REGULAR  4211586 1444302 07-30-13 00:00:00   REGULAR  4212845 1444369
 ", header=T) %>% add_rownames()
 
 write.csv(dat, "data-raw/p33_input1.csv", row.names=FALSE)
 
 df_out = dat %>% 
     # gather to long form
-    gather(var, val, -rowname, -CA, -UNIT) %>% 
+    gather(var, val, -rowname, -CA) %>% 
     # separate ID suffix from variable name by position
     separate(var, c('varNew', 'id')) %>% 
     # spread back to wide form
